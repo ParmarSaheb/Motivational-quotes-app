6ae91cb9570edb97724c9dfac5b3815ba84f3298
NM41 <nayanparmar2190@gmail.com>
2023-07-10 00:48:44 +0530

    create a empty quotes page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quotes/quotes_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    const appTitle  = "Motiv Quotes";
+    const appTitle = "Motiv Quotes";
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: appTitle,
-      theme: ThemeData(       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData(
+        primaryColor: Colors.teal,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.tealAccent),
       ),
       home: const MyHomePage(title: appTitle),
     );
@@ -34,11 +37,19 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(       
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,      
-        title: Text(widget.title),
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(
+          widget.title,
+          style: GoogleFonts.poppins(
+            color: Colors.black,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      body: const Column()
+      body: QuotesPage(),
     );
-  } 
+  }
 }
---
+++ lib/quotes_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuotesPage extends StatefulWidget {
+  const QuotesPage({super.key});
+
+  @override
+  State<QuotesPage> createState() => _QuotesPageState();
+}
+
+class _QuotesPageState extends State<QuotesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello Neon");
+  }
+}
